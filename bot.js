5f8b31c2fa0b5c5d59a97843a4601c244840c41d
croissan <45309528+croissan@users.noreply.github.com>
2018-12-04 21:44:53 +0000

    Add files via upload

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**krwassa porno , xnxx , krwassa By tabonmok , xnxx sex mia5alifa, zebi kbir - سكس عربي ساخن من الطيز الى الطبون **[ " ${x} " ]`)
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -72,7 +72,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**krwassa porno , xnxx , krwassa By tabonmok , xnxx sex mia5alifa, zebi kbir - سكس عربي ساخن من الطيز الى الطبون **[ " ${x} " ]`)
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
